3fee508f90f117048fa7966af05c70354c3d9b86
yefry <yefryrodelo@gmail.com>
2023-08-07 22:20:45 -0500

    segundo ejercicio mejorado

--- Ejercicio2/src/ConversorMonedas.java
+++ Ejercicio2/src/ConversorMonedas.java
@@ -3,18 +3,67 @@ import java.text.DecimalFormat;
 public class ConversorMonedas {
     //2. Conversor de Monedas: Crea una clase ConversorMonedas que contenga métodos para convertir una cantidad de dinero de una moneda a otra, por ejemplo, de dólares a euros o de pesos a dólares.
 
-    public String convertirPesosADolares(double peso) {
-        double dolares = 4140.21;
-        double conversion = peso / dolares;
+    public String pesosColombianosADolares(double peso) {
+        double dolares = 0.00025;
+        double conversion = peso * dolares;
         DecimalFormat df = new DecimalFormat("#.00");
 
         return df.format(conversion);
     }
 
-    public double convertirDolaresAEuros(double dolares) {
-        double euro = 0.9070;
+    public String dolaresAPesosColombiano(double dolar) {
+        double peso = 4028.85;
+        double conversion = dolar * peso;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
+    public String pesoColombianoAPesoMexicano(double peso) {
+        double pesoMexicano = 0.0042;
+        double conversion = peso * pesoMexicano;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
+    public String pesoMexicanoAPesoColombiano(double peso) {
+        double pesoColombiano = 235.43;
+        double conversion = peso * pesoColombiano;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
+    public String pesoColombianoAEuro(double peso) {
+        double euro = 0.00023;
+        double conversion = peso * euro;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
+    public String euroAPesoColombiano(double euro) {
+        double peso = 4426.72;
+        double conversion = peso * euro;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
+    public double dolaresAEuros(double dolares) {
+        double euro = 0.91;
         double conversion = dolares * euro;
         return conversion;
     }
+
+    public String dolaresAPesosMexicanos(double dolar) {
+        double peso = 17.11;
+        double conversion = dolar * peso;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return df.format(conversion);
+    }
+
 
 }
--- Ejercicio2/src/Main.java
+++ Ejercicio2/src/Main.java
@@ -1,17 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     //2. Conversor de Monedas: Crea una clase ConversorMonedas que contenga métodos para convertir una cantidad de dinero de una moneda a otra, por ejemplo, de dólares a euros o de pesos a dólares.
+
+    //NOTA: PARA ESTE EJERCICIO DECIDI TOMAR LA TASA DE CAMBIO DEL DIA 07 DE AGOSTO DEL PRESENTE AÑO(07/08/2023)
     public static void main(String[] args) {
-        // Instanciamos la clase
+       // Instanciamos la clase
         ConversorMonedas conversorMonedas = new ConversorMonedas();
 
-        //1. Convertir dólares a euros
+        /* //1. Convertir dólares a euros
         double dolaresAEuros = conversorMonedas.convertirDolaresAEuros(500);
 
         //2. Convertir Pesos a dolares
         String pesosADolares = conversorMonedas.convertirPesosADolares(1500000);
+        System.out.println("dolares a pesos colombianos: " + conversorMonedas.convertirDolaresAPesos(100));
 
-        System.out.println(dolaresAEuros);
-        System.out.println(pesosADolares);
+        System.out.println("dolares a euros: " + dolaresAEuros);
+        System.out.println("pesos colombianos a dolares " + pesosADolares); */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el tipo de moneda que deseas hacerla la conversion(COP, MXN, USD, EUR)");
+        String moneda = scanner.nextLine();
+        String monedaAConvertir = moneda.toUpperCase();
+
+        System.out.println("Escribe el valor a convertir");
+        double valorMonedaAConvertir = scanner.nextDouble();
+
+        System.out.println("Ten en cuenta que el COP al ser la moneda nacional y el USD al ser la internacional mas importante, son las únicas que tendran conversiones a las otras 3 monedas. Pero el MXN y el EUR solo tendran conversiones al COP, al ser esta la moneda nacional");
+
+        switch (monedaAConvertir) {
+            case "COP":
+                System.out.println("La moneda :" + valorMonedaAConvertir + "COP, tiene las siguientes conversiones");
+                System.out.println("Euro: " + conversorMonedas.pesoColombianoAEuro(valorMonedaAConvertir));
+                System.out.println("Peso Mexicano: " + conversorMonedas.pesoColombianoAPesoMexicano(valorMonedaAConvertir));
+                System.out.println("Dolar: " +conversorMonedas.pesosColombianosADolares(valorMonedaAConvertir));
+                break;
+            case "MXN":
+                System.out.println("La moneda :" + valorMonedaAConvertir + "MXN, tiene las siguientes conversiones");
+                System.out.println("Peso Colombiano: " + conversorMonedas.pesoMexicanoAPesoColombiano(valorMonedaAConvertir));
+                break;
+            case "USD":
+                System.out.println("La moneda :" + valorMonedaAConvertir + "USD, tiene las siguientes conversiones");
+                System.out.println("Peso Colombiano: " + conversorMonedas.dolaresAPesosColombiano(valorMonedaAConvertir));
+                System.out.println("Euro: " + conversorMonedas.dolaresAEuros(valorMonedaAConvertir));
+                System.out.println("Peso Mexicano: " + conversorMonedas.dolaresAPesosMexicanos(valorMonedaAConvertir));
+                break;
+            case "EUR":
+                System.out.println("La moneda :" + valorMonedaAConvertir + "EUR, tiene las siguientes conversiones");
+                System.out.println("Peso Colombiano: " + conversorMonedas.euroAPesoColombiano(valorMonedaAConvertir));
+                break;
+        }
+
+        /* System.out.println("Escribe el tipo de moneda a la que deseas convertir el valor anterior (peso colombiano, peso mexicano, dolar, euro)");
+        String tipoDeMonedaAConvertir = scanner.nextLine(); */
+
+
 
     }
 }
