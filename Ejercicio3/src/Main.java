c574c5f9dfe931631329153c80bffd63ff2a9caf
yefry <yefryrodelo@gmail.com>
2023-08-07 22:30:10 -0500

    tercer ejercicio mejorado

--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     // 3. Calculadora Simple: Crea una clase Calculadora que contenga métodos para realizar operaciones matemáticas básicas, como suma, resta, multiplicación y división.
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
 
-        System.out.println(calculadora.suma(2,2));
-        System.out.println(calculadora.resta(2,2));
-        System.out.println(calculadora.multiplicacion(2,2));
-        System.out.println(calculadora.division(2,2));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digita el primer numero");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digita el segundo numero");
+        int num2 = scanner.nextInt();
+
+        System.out.println( num1 + " + " + num2 + " = " + calculadora.suma(num1,num2));
+        System.out.println( num1 + " - " + num2 + " = " + calculadora.resta(num1,num2));
+        System.out.println( num1 + " * " + num2 + " = " + calculadora.multiplicacion(num1,num2));
+        System.out.println( num1 + " / " + num2 + " = " + calculadora.division(num1,num2));
 
     }
 }
